fb59e235704cac9151a4dc59c732a5b1e800a98d
sawayamakouji <sawayamakouji@gmail.com>
2025-01-15 05:50:08 +0900

    ZA

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-// 開発環境かGitHub Pagesかを判別
-const isGithubPages = process.env.NODE_ENV === 'production';
-
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -16,12 +13,11 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  base: isGithubPages ? '/sl_one2/' : './', // GitHub Pagesの場合のみbaseを設定
+  // base: './', // または base設定自体を削除
 
   build: {
     rollupOptions: {
-      external: ['react-router-dom'], // ここにreact-router-domを追加
+      external: ['react-router-dom'],
     },
   },
-
 });
